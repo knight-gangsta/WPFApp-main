eb6f3f5e80a9f8ba55d8ece8cdfeb42288e7b6b7
guasonsad <guasonsad@GUASON-KING>
2024-04-10 11:19:07 -0500

    INICIO

--- WPFApp-main/WPFApp/MainWindow.xaml.cs
+++ WPFApp-main/WPFApp/MainWindow.xaml.cs
@@ -1,1 +1,84 @@
-﻿
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			CreateTask();
+		}
+
+		void AddMessage(string message)
+		{
+			int CurrentThreadId = Thread.CurrentThread.ManagedThreadId;
+			this.Dispatcher.Invoke(() =>
+			{
+				Message.Content +=
+				   $"Mensaje: {message}, " +
+				   $"Hilo Actual: {CurrentThreadId}\n";
+			});
+		}
+
+		void CreateTask()
+		{
+			Task T;
+			// ¿Qué es un delegado? Apuntadores a funciones.
+			// EJEMPLO: Action y Func
+			Action Code = new Action(ShowMessage);
+			T = new Task(Code);
+
+			Task T2 = new Task(delegate
+			{
+				MessageBox.Show("Ejecutando una tarea en un método anónimo.");
+			}
+			);
+
+			//Expresión Lambda:
+			//(parámetros de entrada) => Expresión
+			//EJ: () => Expresión
+			// El operador lambda (=>) se lee como "Va Hacia"
+
+			Task T3A = new Task(ShowMessage);
+			Task T3 = new Task(() => ShowMessage());
+
+			Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+			Task T5 = new Task(() =>
+			{
+				DateTime CurrentDate = DateTime.Today;
+				DateTime StartDate = CurrentDate.AddDays(30);
+				MessageBox.Show($"Tarea 5. Fecha Calculada: {StartDate}");
+			}
+				);
+
+			Task T6 = new Task((message) =>
+			MessageBox.Show(message.ToString()), "Expresión Lambda con parámetros");
+		}
+		Task T7 = new Task(() => AddMessage("Ejecutando la tarea."));
+		T7.Start();
+
+            AddMessage("En El Hilo Principal");
+	}
+
+	void ShowMessage()
+	{
+		MessageBox.Show("Ejecutando el método ShowMessage");
+	}
+
+}
+}
+
